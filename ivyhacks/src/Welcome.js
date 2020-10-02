060cb6c02c3bc81aa1b0b85546b27e8c89d5eb9b
Kincent <kinlan19@bergen.org>
2020-10-02 16:41:32 -0400

    some login and signup things

--- ivyhacks/src/Welcome.js
+++ ivyhacks/src/Welcome.js
@@ -1,12 +1,12 @@
 import React from "react";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import "./css/welcome.css";
 
 const Welcome = () => {
   return (
       <div id="welcome">
-          <button id="login"><Link to="/login"><label>Log in</label></Link></button>
-          <button id="signup"><Link to="/signup"><label>Sign up</label></Link></button>
+          <Link to="/login"><label>Log in</label></Link>
+          <Link to="/signup"><label>Sign up</label></Link>
       </div>
   )
 };
