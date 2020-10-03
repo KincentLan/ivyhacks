ea25a2397d302c2d1e836b4a1c9a33321c18322a
Richard <jsksosdgkd29y@gmail.com>
2020-10-03 12:41:25 -0400

    added displaying roles

--- ivyhacks/src/application/Home.js
+++ ivyhacks/src/application/Home.js
@@ -5,10 +5,12 @@ import {AuthContext} from "../auth/Auth.js";
 
 const Home = () => {
     const [courses, setCourses] = useState([]);
+    const [role, setRole] = useState([]);
     const {currentUser} = useContext(AuthContext);
 
     useEffect(() => {
-        app.database().ref('users/' + currentUser.uid).once('value').then(function (snapshot) {
+        app.database().ref('users/' + currentUser.uid).once('value').then(
+          function (snapshot) {
             const courses = snapshot.val()['classes'];
             if (courses.length > 0) {
                 setCourses(courses);
@@ -16,11 +18,19 @@ const Home = () => {
         })
     }, []);
 
+    useEffect(() => {
+      app.database().ref('users/' + currentUser.uid).once('value').then(
+        function (snapshot) {
+          setRole(snapshot.val()['userType']);
+      })
+  }, []);
+
     return (
         <div className="dashboard">
             {courses.length > 0 && (
                 <div className="courses">
                     <h1>Home</h1>
+                    <div className='role'>{ role }</div>
                     {courses.map((value, i) => {
                         return <div className="class">{value}</div>
                     })}
