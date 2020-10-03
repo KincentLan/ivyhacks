761c90c383f7b6c4027565fdc243b372e706115a
Kincent <kinlan19@bergen.org>
2020-10-03 01:40:57 -0400

    css frontend work

--- ivyhacks/src/Welcome.js
+++ ivyhacks/src/Welcome.js
@@ -8,7 +8,6 @@ const Welcome = () => {
                 <button id="loginButton" className="wellink"><Link className="white" to="/login">Log in</Link></button>
                 <button id="signupButton" className="wellink"><Link className="white" to="/signup">Sign up</Link></button>
             </div>
-            <div id="description">We help connect you with your peers</div>
         </div>
     )
 };
