8511cef1e9ffd0e60dd116bd9049259e52e5c233
Richard <jsksosdgkd29y@gmail.com>
2020-10-03 17:54:08 -0400

    now instructors can add classes

--- ivyhacks/src/base.js
+++ ivyhacks/src/base.js
@@ -1,15 +1,17 @@
 import * as firebase from "firebase/app";
 import "firebase/auth";
 
-const app = firebase.initializeApp({
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
-  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
-});
+const firebaseConfig = {
+  apiKey: "AIzaSyDFxy8I3Q-N3EUEVOGWoGya1pxL0x35npg",
+  authDomain: "ivyhacks-b3547.firebaseapp.com",
+  databaseURL: "https://ivyhacks-b3547.firebaseio.com",
+  projectId: "ivyhacks-b3547",
+  storageBucket: "ivyhacks-b3547.appspot.com",
+  messagingSenderId: "167248186439",
+  appId: "1:167248186439:web:8031f0d3fe2d1efdab3562",
+  measurementId: "G-Q0T35K7J2H"
+};
+
+const app = firebase.initializeApp(firebaseConfig);
 
 export default app;
