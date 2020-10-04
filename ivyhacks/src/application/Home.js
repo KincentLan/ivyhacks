55619c431c1a325ff6115e698ab77ee1371ec7f5
Kincent <kinlan19@bergen.org>
2020-10-04 10:41:33 -0400

    css frontend stuff

--- ivyhacks/src/application/Home.js
+++ ivyhacks/src/application/Home.js
@@ -3,6 +3,7 @@ import app from "../base";
 import firebase from 'firebase/app';
 import {useAuthState} from 'react-firebase-hooks/auth';
 import 'firebase/auth';
+import '../css/home.css';
 import {Link} from "react-router-dom"
 
 const Home = () => {
@@ -36,7 +37,6 @@ const Home = () => {
         return (
             <div id="dashboard">
                 <div className="navbar">
-
                     <div className="dropdown">
                         <button className="dropbtn"> {name} </button>
                         <div className="dropdown-content">
@@ -47,22 +47,23 @@ const Home = () => {
                     </div>
                 </div>
 
-                <div>
-                    <p>
-                        <div id="section_label">Courses you manage</div>
-                    </p>
-                </div>
                 <div id='classes'>
-                    {courses && (
+                    <h1>Courses you manage</h1>
+                    <div className="wrapper"> {courses && (
                         courses.map((value) => {
-                            return <button id="class_button"><Link className="black"
-                                                                   to={"/course/" + value}>{value}</Link></button>;
+                            return <div className="option">
+                                <button id="class_button"><Link className="black"
+                                                                to={"/course/" + value}>{value}</Link></button>
+                            </div>;
                         })
 
                     )}
-                    <button id='new_class_button'>
-                        <Link className="black" to="/CreateClass">+ Create Another Class</Link>
-                    </button>
+                        <div className="option">
+                            <button id='new_class_button'>
+                                <Link className="black" to="/CreateClass">+ Create Another Class</Link>
+                            </button>
+                        </div>
+                    </div>
                 </div>
 
             </div>)
@@ -70,20 +71,30 @@ const Home = () => {
         return (
             <div id="dashboard">
                 <div className="navbar">
-                    <button id="homeButton"><Link className="white" to="/">Home</Link></button>
-                    <label id="name"> {name} </label>
-                    <button id="homeButton" onClick={() => app.auth().signOut()}>Sign out</button>
+                    <div className="dropdown">
+                        <button className="dropbtn"> {name} </button>
+                        <div className="dropdown-content">
+                            <a href="#">
+                                <button id="homeButton" onClick={() => app.auth().signOut()}>Sign out</button>
+                            </a>
+                        </div>
+                    </div>
                 </div>
+
                 <div id='classes'>
                     <h1>Courses you are in</h1>
-                    <div></div>
-                    {courses && (
+                    <div className="wrapper"> {courses && (
                         courses.map((value) => {
-                            return <button id="class_button"><Link className="black"
-                                                                   to={"/course/" + value}>{value}</Link></button>;
+                            return <div className="option">
+                                <button id="class_button"><Link className="black"
+                                                                to={"/course/" + value}>{value}</Link></button>
+                            </div>;
                         })
+
                     )}
+                    </div>
                 </div>
+
             </div>)
     }
 }
