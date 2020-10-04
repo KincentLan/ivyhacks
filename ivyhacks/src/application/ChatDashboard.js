49b67821301960ea96020a5910cffaff942c4aab
Kincent <kinlan19@bergen.org>
2020-10-04 00:40:35 -0400

    changed auth stuff

--- ivyhacks/src/application/ChatDashboard.js
+++ ivyhacks/src/application/ChatDashboard.js
@@ -8,12 +8,53 @@ import 'firebase/auth';
 
 const ChatDashboard = () => {
     const auth = firebase.auth();
-    const uid = auth.user.uid;
-    const [courses, setCourses] = useState([]);
-    app.database().ref('users')
+    const [currentUser] = useAuthState(auth);
+
+    const {id} = useParams();
+    const [redirect, setRedirect] = useState(false);
+    const [assignments, setAssignments] = useState([]);
+    const [loaded, setLoaded] = useState(false);
+
+    useEffect(() => {
+        if (!loaded) {
+            app.database().ref('users/' + currentUser.uid).once('value').then((snapshot) => {
+                const dbUser = snapshot.val();
+                if (dbUser['classes'] === undefined || !dbUser['classes'].includes(id.toUpperCase())) {
+                    setRedirect(true);
+                    setLoaded(true);
+                }
+            })
+
+            app.database().ref('classes/' + id.toUpperCase()).on('value', (snapshot) => {
+                const curAssignments = [];
+                console.log(snapshot.val());
+                console.log('this ran');
+                if (snapshot.val() !== null) {
+                    const dbAssignments = snapshot.val()['assignments'];
+                    for (const key in dbAssignments) {
+                        curAssignments.push({assignment: key, question: dbAssignments[key]['question']});
+                    }
+                }
+                setAssignments(curAssignments);
+            });
+        };
+    }, []);
+
+
+
+    console.log(assignments);
+
+    if (redirect) {
+        return <Redirect to="/home"/>;
+    }
+
     return (
-        <div>Hello</div>
-    );
-};
+        <div>{assignments.map((value) => {
+            return <div className="row"> {value.question.map((q) => {
+                return <div className="assignment">{value.assignment} <div className="question"> {q} </div> </div>;
+            })} </div>
+        })}
+        </div>);
+}
 
 export default ChatDashboard;
