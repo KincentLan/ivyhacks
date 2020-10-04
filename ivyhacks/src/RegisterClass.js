a5abb762b91474681b4c647627950efa59930166
albeartoz <zhang.alb@husky.neu.edu>
2020-10-04 00:27:42 -0400

    gaming

--- ivyhacks/src/RegisterClass.js
+++ ivyhacks/src/RegisterClass.js
@@ -1,10 +1,32 @@
 import React from "react";
 import app from "./base";
+import {useAuthState} from 'react-firebase-hooks/auth';
 import {AuthContext} from "./auth/Auth.js";
 import {Redirect} from "react-router";
 
 const RegisterClass = () => {
-    
+    const auth = app.auth();
+    const [currentUser] = useAuthState(auth);
+
+    const addUserToClass = (async event => {
+        event.preventDefault()
+        const {className} = event.target.elements;
+        await app.database().ref("users/" + currentUser.uid + "/classes/").push().set({
+            className : className.value
+        })
+    })
+
+    return (
+        <div>
+        <h1>Register yourself to a class</h1>
+        <form onSubmit={addUserToClass}>
+        <label>
+        Class Name
+        <input name="className" placeholder="PHYS 2020"/>
+        </label>
+        </form>
+        </div>
+    )
 }
 
 export default RegisterClass 
