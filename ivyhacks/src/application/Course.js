41d8c502b59aaf5768e33255086fd375e4ae3120
Richard <jsksosdgkd29y@gmail.com>
2020-10-04 01:38:40 -0400

    added files that i forgot to add

---
+++ ivyhacks/src/application/Course.js
@@ -0,0 +1,17 @@
+import React, {useEffect, useState} from "react";
+import app from "../base";
+import firebase from 'firebase/app';
+import {useAuthState} from 'react-firebase-hooks/auth';
+import 'firebase/auth';
+import {Link} from "react-router-dom"
+
+const Course = () => {
+    
+
+    return <div>
+
+    </div>
+    
+};
+
+export default Course
