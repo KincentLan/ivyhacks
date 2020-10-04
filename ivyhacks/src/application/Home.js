263aaf1dc074f337cd6d2041a69fbb7700fe4810
Richard <jsksosdgkd29y@gmail.com>
2020-10-03 21:34:57 -0400

    fixed

--- ivyhacks/src/application/Home.js
+++ ivyhacks/src/application/Home.js
@@ -3,7 +3,6 @@ import app from "../base";
 import firebase from 'firebase/app';
 import {useAuthState} from 'react-firebase-hooks/auth';
 import 'firebase/auth';
-import {useAuthState} from 'react-firebase-hooks/auth';
 import CreateClass from "../CreateClass"
 
 const Home = () => {
